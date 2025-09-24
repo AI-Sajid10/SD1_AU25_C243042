b238c2457c090e2617da28e9471312a70fdebb2e
Sajid <c243042@ugrad.iiuc.ac.bd>
2025-09-24 12:09:58 +0600

    Student class oop

--- Practice_OOP/src/Student.java
+++ Practice_OOP/src/Student.java
@@ -3,4 +3,10 @@ public class Student {
     String name;
     int age,id;
     double fee;
+    void displayInfo(){
+        System.out.println("Student name:"+name);
+        System.out.println("Student id:"+id);
+        System.out.println("Student age:"+age);
+        System.out.println("Student fee :"+fee);
+    }
 }
--- Practice_OOP/src/Student_main.java
+++ Practice_OOP/src/Student_main.java
@@ -2,18 +2,21 @@ public class Student_main {
     public static void main(String args[]){
         Student s1 = new Student();
         s1.name = "Ahmed Ibrahim Sajid";
+        s1.id = 243042;
         s1.age = 19;
         s1.fee = 38.5;
-        System.out.println(s1.name);
+        /*System.out.println(s1.name);
         System.out.println(s1.age);
-        System.out.println(s1.fee);
-
+        System.out.println(s1.fee);*/
+        s1.displayInfo(); //by using a method name displayInfo
         Student s2 = new Student();
         s2.name = "Leo Messi";
+        s2.id = 223010;
         s2.age = 30;
         s2.fee = 100.00;
-        System.out.println(s2.name);
+        /*System.out.println(s2.name);
         System.out.println(s2.age);
-        System.out.println(s2.fee);
+        System.out.println(s2.fee); */
+        s2.displayInfo(); //by using a method name displayInfo
     }
 }
