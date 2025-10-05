71c54eea2a2759efabcc010bb7bf857a6083b366
Sajid <c243042@ugrad.iiuc.ac.bd>
2025-10-05 22:13:06 +0600

    Math funtion in java

---
+++ Practice_OOP/src/MathExample.java
@@ -0,0 +1,15 @@
+public class MathExample {
+    public static void main(String[] args){
+
+        System.out.println(Math.abs(-9.5));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.PI);
+        System.out.println(Math.log(1));
+        System.out.println(Math.max(-2.3,-15.6));
+        System.out.println(Math.min(100,40));
+        System.out.println(Math.ceil(8.14));
+        System.out.println(Math.floor(8.14));
+    }
+
+}
