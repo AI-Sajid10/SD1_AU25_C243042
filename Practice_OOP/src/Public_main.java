cfc60ed84c3104f9cf03dfbea474504bee174095
Sajid <c243042@ugrad.iiuc.ac.bd>
2025-10-07 16:58:59 +0600

    Setter and Getter Methods

---
+++ Practice_OOP/src/Public_main.java
@@ -0,0 +1,15 @@
+public class Public_main {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.setName("Ahmed Ibrahim Sajid");
+        p1.setAge(20);
+        System.out.println(p1.getAge());
+        System.out.println(p1.getName());
+
+        Person p2 = new Person();
+        p2.setName("Leo Messi");
+        p2.setAge(30);
+        System.out.println(p2.getAge());
+        System.out.println(p2.getName());
+    }
+}
